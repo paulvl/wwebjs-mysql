58137aa7495a0726475d0a29472d4d9b0dfa35f6
Paul Vidal <paul.vidal.lujan@gmail.com>
2023-09-02 01:38:01 -0500

    fixed save on existing session record

--- src/MysqlStore.js
+++ src/MysqlStore.js
@@ -18,7 +18,12 @@ class MysqlStore {
     async save(options) {
         const connection = await this.pool.getConnection();
         const fileBuffer = fs.readFileSync(`${options.session}.zip`);
-        await connection.execute(`INSERT INTO \`${this.tableInfo.table}\` (\`${this.tableInfo.session_column}\`, \`${this.tableInfo.data_column}\`) VALUES (?, ?)`, [options.session, fileBuffer]);
+        const [rows] = await connection.execute( `SELECT  as count FROM \`${this.tableInfo.table}\` WHERE \`${this.tableInfo.session_column}\` = ?`, [options.session]);
+        if (rows[0].count == 0) {
+            await connection.execute(`INSERT INTO \`${this.tableInfo.table}\` (\`${this.tableInfo.session_column}\`, \`${this.tableInfo.data_column}\`) VALUES (?, ?)`, [options.session, fileBuffer]);
+        } else {
+            await connection.execute(`UPDATE \`${this.tableInfo.table}\` SET \`${this.tableInfo.data_column}\` = ? WHERE \`${this.tableInfo.session_column}\` = ?`, [fileBuffer, options.session]);
+        }
         connection.release();
     }
 
